3f94ae4eacda2cdadb18332a2dc062963eaa1bb2
Jacob Bleser <jacobbleser@aol.com>
2016-09-18 23:13:58 +0000

    Added moment.js, added server response for date parameters

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res){
+    res.send("Psst... Put in a date/timestamp...");
+});
+
+app.get('/:timestamp', function(req, res){
+   var input = req.params.timestamp;
+   var json = {
+       "unix": null,
+       "natural": null
+   };
+   
+   
+   // Check if input is a unix timestamp
+   if (moment.unix(input).isValid()){
+       
+       json.unix = input;
+       json.natural = moment(input).format('MMMM D, YYYY');
+       app.send(json);
+   }
+   
+   // Check if input is a normal date
+   else if (moment(input).isValid()){
+       var unixTime = (new Date(input).getTime())/1000;
+       json.unix = unixTime;
+       json.natural = input;
+       app.send(json);
+   }
+   else {
+       app.send(json);
+   }
 });
 
 
-var port = (process.env.PORT || 8080)
-app.listen(port);
+app.listen(process.env.PORT || 8080);
